49a9baee1e5a3a640446c794c5b2b385bc3bf302
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-21 22:31:47 +0100

    Get first Apod from dashboard component using NasaService

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NasaService } from '../services/nasa.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  apods: any[] = [];
+
+  constructor(
+    private nasaService: NasaService
+  ) { }
 
   ngOnInit(): void {
+    this.nasaService.getApod(new Date()).subscribe(apod => {
+      this.apods.push(apod);
+    });
   }
 
 }
--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { CardComponent } from './components/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -11,7 +13,9 @@ import { CardComponent } from './components/card/card.component';
     CardComponent
   ],
   imports: [
-  CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
   ]
