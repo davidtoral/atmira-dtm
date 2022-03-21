4afea664ffae5628f6455ee8aa2698215f860de1
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-21 22:31:23 +0100

    Card component: Receive Apod as an Input

--- src/app/main/dashboard/components/card/card.component.ts
+++ src/app/main/dashboard/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Apod } from 'src/app/interfaces/apod';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  @Input() apod: Apod;
 
   constructor() { }
 
