b593cd8362e6d80e4d3381cb4e43705d833bf624
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-22 21:34:58 +0100

    Add detail component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { DetailComponent } from './main/detail/detail.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent}
+  { path: '', component: DashboardComponent },
+  { path: 'detail/:id', component: DetailComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
