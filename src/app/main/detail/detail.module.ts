b492af6a6eece30302597ee45ca1800dba75c6fa
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-22 22:31:27 +0100

    Detail component html

--- src/app/main/detail/detail.module.ts
+++ src/app/main/detail/detail.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailComponent } from './detail.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -9,6 +11,8 @@ import { DetailComponent } from './detail.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    MatButtonModule
   ],
   exports: [
   ]
