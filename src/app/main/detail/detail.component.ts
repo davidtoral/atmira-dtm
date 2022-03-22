43ed4a97f1d15ccb30009c0ba500f06cd5e6904d
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-22 21:51:43 +0100

    Get apod in detail component

--- src/app/main/detail/detail.component.ts
+++ src/app/main/detail/detail.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Apod } from 'src/app/interfaces/apod';
+import { NasaService } from '../services/nasa.service';
+import { takeUntil } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject();
 
-  constructor() { }
+  apod: Apod;
+
+  constructor(
+    private nasaService: NasaService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getApodData();
   }
 
+  getApodData(): void {
+    const date = new Date(this.route.snapshot.paramMap.get('date'));
+
+    this.nasaService.getApod(date)
+    .pipe(takeUntil(this.unsubscribe$)).subscribe(apod => {
+      this.apod = apod;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
