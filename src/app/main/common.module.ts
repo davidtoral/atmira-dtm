3bec2f92ff5103fb210c3e1a84344f126cf51b80
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-22 21:34:50 +0100

    Create detail component

--- src/app/main/common.module.ts
+++ src/app/main/common.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { NasaService } from './services/nasa.service';
+import { DetailModule } from './detail/detail.module';
 
 
 
@@ -9,7 +10,8 @@ import { NasaService } from './services/nasa.service';
   declarations: [],
   imports: [
     CommonModule,
-    DashboardModule
+    DashboardModule,
+    DetailModule
   ],
   providers: [
     NasaService,
