e246fd717c7de99b9c52d4ca4bf4b177f6196f3b
davidtoral <davidtoralmoreno98@gmail.com>
2022-03-23 22:08:52 +0100

    Remove unused declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppCommonModule } from './main/common.module';
 import { UtilsModule } from './utils/utils/utils.module';
 
